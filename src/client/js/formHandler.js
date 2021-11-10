25edb82049e917e819e69eb158fb4bd46af5ec9f
Jimmy Dale <jimmy.dale@warnermedia.com>
2021-11-10 17:00:15 -0500

    completed lesson

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-function handleSubmit(event) {
+ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
@@ -12,3 +12,5 @@ function handleSubmit(event) {
         document.getElementById('results').innerHTML = res.message
     })
 }
+
+export {handleSubmit}
